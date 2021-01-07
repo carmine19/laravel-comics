7f7a35dcabd075ae60c8c906a1df841da0969103
carmine <carminepepiciello@gmail.com>
2021-01-07 23:22:54 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,116 @@
-footer
+<footer>
+
+        <div class="f-top">
+
+            <div class="box-image">
+                <img src="{{asset('images/dc-logo-bg.png')}}" alt="">
+            </div>
+            <div class="container">
+                <div class="main-box">
+                    <div class="box-1">
+                         <div class="title">
+                        <h3>Dc comics</h3>
+                        </div>
+                        <div class="box-link">
+                            <ul>
+                                <li><a href="">Characters</a></li>
+                                <li><a href="">Comics</a></li>
+                                <li><a href="">Movies</a></li>
+                                <li><a href="">TV</a></li>
+                                <li><a href="">Games</a></li>
+                                <li><a href="">Videos</a></li>
+                                <li><a href="">News</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="box-2">
+                         <div class="title">
+                        <h3>Dc comics</h3>
+                        </div>
+                        <div class="box-link">
+                            <ul>
+                                <li><a href="">Shop DC</a></li>
+                                <li><a href="">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="main-box">
+                    <div class="title">
+                        <h3>Dc comics</h3>
+                    </div>
+                    <div class="box-link">
+                        <ul>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="main-box">
+                    <div class="title">
+                        <h3>Dc comics</h3>
+                    </div>
+                    <div class="box-link">
+                        <ul>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                            <li><a href="">link</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="box-text-end">
+                    <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="">noted here</a>. All rights reserved. </p>
+                    <a href="" class="link-cookie">Cookie Setting</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="f-bottom">
+            <div class="container">
+                <div class="main-box">
+                    <div class="box-left">
+                        <div class="box-btn">
+                            <a href="" class="btn">Sign-Up Now!</a>
+                        </div>
+                    </div>
+                    <div class="box-right">
+                        <div class="main-box">
+                            <div class="box-text">
+                                <p>follow us</p>
+                            </div>
+                            <div class="box-social">
+                                <div class="box-img">
+                                    <img src="{{asset('images/footer-facebook.png')}}" alt="">
+                                </div>
+                                <div class="box-img">
+                                    <img src="{{asset('images/footer-periscope.png')}}" alt="">
+                                </div>
+                                <div class="box-img">
+                                    <img src="{{asset('images/footer-pinterest.png')}}" alt="">
+                                </div>
+                                <div class="box-img">
+                                    <img src="{{asset('images/footer-twitter.png')}}" alt="">
+                                </div>
+                                <div class="box-img">
+                                    <img src="{{asset('images/footer-youtube.png')}}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+</footer>
